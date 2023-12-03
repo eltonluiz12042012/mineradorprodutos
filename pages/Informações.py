db8660709e7ec4f159f3dcf905de1f9595eac649
Elton Luiz dos Santos do Franco <eltonluiz12042012@gmail.com>
2023-12-03 17:50:59 -0300

    Create Informações.py

---
+++ pages/Informações.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import requests
+import pandas as pd
+import plotly.express as px   
+
+st.markdown("""1. **Facilidade de Acesso:**
+   - *Mobilidade*: Aplicativos para consulta de produtos oferecem acesso instantâneo a informações de afiliados em qualquer lugar, tornando o processo mais ágil e flexível.
+   - *Interface Intuitiva*: Geralmente, os apps são projetados com interfaces amigáveis, proporcionando uma experiência de usuário simplificada e fácil de usar.
+
+2. **Atualizações em Tempo Real:**
+   - *Informações Atualizadas*: Aplicativos podem fornecer dados em tempo real sobre produtos, preços e comissões, permitindo que afiliados tomem decisões informadas com base nas últimas informações disponíveis.
+
+3. **Gestão Eficiente de Campanhas:**
+   - *Acompanhamento de Desempenho*: Muitos aplicativos oferecem recursos para rastrear o desempenho de campanhas de afiliados, fornecendo métricas detalhadas e relatórios que facilitam a otimização de estratégias.
+   """)
